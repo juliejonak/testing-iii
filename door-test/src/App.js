2494ab47a5e98ffad9b2731dada3ad99bc7b11c1
Julie Jonak <juliejonak10@gmail.com>
2019-02-04 21:17:41 -0600

    Pseudo coded test cases

--- door-test/src/App.js
+++ door-test/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Controls from './components/controls';
 import Dashboard from './components/dashboard';
-import Display from './components/display';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Display />
+        <Dashboard />
       </div>
     );
   }
--- door-test/src/App.test.js
+++ door-test/src/App.test.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Dashboard from './components/dashboard';
+import Controls from './components/controls';
+import Display from './components/display';
 
 
 it('renders without crashing', () => {
   shallow(<App />)
 });
+
+//DISPLAY
+// - displays if gate is open/closed and if it is locked/unlocked
+// - displays 'Closed' if the `closed` prop is `true` and 'Open' if otherwise
+// - displays 'Locked' if the `locked` prop is `true` and 'Unlocked' if othewise
+// - when `locked` or `closed` use the `red-led` class
+// - when `unlocked` or `open` use the `green-led` class
+
+//GATE
+// gate defaults to `unlocked` and `open`
+// gate cannot be closed or opened if it is locked
+
+//CONTROLS
+// - provide buttons to toggle the `closed` and `locked` states.
+// - buttons' text changes to reflect the state the door will be in if clicked
+// - the closed toggle button is disabled if the gate is closed
+// - the locked toggle button is disabled if the gate is open
+
+//DASHBOARD
+// renders the controls and display
