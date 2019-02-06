a1e4aace6d5bc8d9575140d2accce096adffdfed
Julie Jonak <juliejonak10@gmail.com>
2019-02-05 20:51:24 -0600

    Added dashboard tests

--- door-test/src/App.test.js
+++ door-test/src/App.test.js
@@ -11,86 +11,99 @@ it('renders without crashing', () => {
 });
 
 
-
 describe('dashboard', () => {
 
-  it('renders the control and display components', () => {
-
-  })
-})
-
-
-
-describe('display', () => {
-
   it('renders without crashing', () => {
-    shallow(<Display />)
+    shallow(<Dashboard />)
   })
 
-  it('default renders false props', () => {
+  it('renders the display component', () => {
+    const wrapper = shallow(<Dashboard />);
+    const display = wrapper.find('Display');
 
+    expect(display.length).toBe(1);
   })
 
-  it('renders the display panel', () => {
+  it('renders the control component', () => {
+    const wrapper = shallow(<Dashboard />);
+    const controls = wrapper.find('Controls');
 
-  })
-
-  it('displays "Closed" if the closed prop is true', () => {
-
-  })
-
-  it('displays "Open" if the closed prop is false', () => {
-
-  })
-
-  it('uses the "red-led" class when "locked"', () => {
-
-  })
-
-  it('uses the "red-led" class when "closed"', () => {
-    
-  })
-
-  it('uses the "green-led" class when "open"', () => {
-    
-  })
-
-  it('uses the "green-led" class when "unlocked"', () => {
-    
-  })
-
-})
-
-
-
-describe('gate', () => {
-
-  it('defaults to "unlocked" and "open"', () => {
-
-  })
-
-  it('does not close or open if the gate is locked', () => {
-
+    expect(controls.length).toBe(1);
   })
 })
 
 
 
-describe('controls', () => {
+// describe('display', () => {
 
-  it('renders state for the closed and locked buttons', () => {
+  // it('renders without crashing', () => {
+  //   shallow(<Display />)
+  // })
 
-  })
+//   it('default renders false props', () => {
 
-  it('changes the test of the button when the state changes', () => {
+//   })
 
-  })
+//   it('renders the display panel', () => {
 
-  it('disables the close toggle button when the gate is closed', () => {
+//   })
 
-  })
+//   it('displays "Closed" if the closed prop is true', () => {
 
-  it('disables the lock toggle button when the gate is open', () => {
+//   })
 
-  })
-})
+//   it('displays "Open" if the closed prop is false', () => {
+
+//   })
+
+//   it('uses the "red-led" class when "locked"', () => {
+
+//   })
+
+//   it('uses the "red-led" class when "closed"', () => {
+    
+//   })
+
+//   it('uses the "green-led" class when "open"', () => {
+    
+//   })
+
+//   it('uses the "green-led" class when "unlocked"', () => {
+    
+//   })
+
+// })
+
+
+
+// describe('gate', () => {
+
+//   it('defaults to "unlocked" and "open"', () => {
+
+//   })
+
+//   it('does not close or open if the gate is locked', () => {
+
+//   })
+// })
+
+
+
+// describe('controls', () => {
+
+//   it('renders state for the closed and locked buttons', () => {
+
+//   })
+
+//   it('changes the test of the button when the state changes', () => {
+
+//   })
+
+//   it('disables the close toggle button when the gate is closed', () => {
+
+//   })
+
+//   it('disables the lock toggle button when the gate is open', () => {
+
+//   })
+// })
