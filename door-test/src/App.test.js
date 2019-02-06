86fd2628b26f7836d7f5b7a61f4aa1ec7853238e
Julie Jonak <juliejonak10@gmail.com>
2019-02-05 20:38:39 -0600

    Pseudocode out tests

--- door-test/src/App.test.js
+++ door-test/src/App.test.js
@@ -10,22 +10,87 @@ it('renders without crashing', () => {
   shallow(<App />)
 });
 
-//DISPLAY
-// - displays if gate is open/closed and if it is locked/unlocked
-// - displays 'Closed' if the `closed` prop is `true` and 'Open' if otherwise
-// - displays 'Locked' if the `locked` prop is `true` and 'Unlocked' if othewise
-// - when `locked` or `closed` use the `red-led` class
-// - when `unlocked` or `open` use the `green-led` class
 
-//GATE
-// gate defaults to `unlocked` and `open`
-// gate cannot be closed or opened if it is locked
 
-//CONTROLS
-// - provide buttons to toggle the `closed` and `locked` states.
-// - buttons' text changes to reflect the state the door will be in if clicked
-// - the closed toggle button is disabled if the gate is closed
-// - the locked toggle button is disabled if the gate is open
+describe('dashboard', () => {
 
-//DASHBOARD
-// renders the controls and display
+  it('renders the control and display components', () => {
+
+  })
+})
+
+
+
+describe('display', () => {
+
+  it('renders without crashing', () => {
+    shallow(<Display />)
+  })
+
+  it('default renders false props', () => {
+
+  })
+
+  it('renders the display panel', () => {
+
+  })
+
+  it('displays "Closed" if the closed prop is true', () => {
+
+  })
+
+  it('displays "Open" if the closed prop is false', () => {
+
+  })
+
+  it('uses the "red-led" class when "locked"', () => {
+
+  })
+
+  it('uses the "red-led" class when "closed"', () => {
+    
+  })
+
+  it('uses the "green-led" class when "open"', () => {
+    
+  })
+
+  it('uses the "green-led" class when "unlocked"', () => {
+    
+  })
+
+})
+
+
+
+describe('gate', () => {
+
+  it('defaults to "unlocked" and "open"', () => {
+
+  })
+
+  it('does not close or open if the gate is locked', () => {
+
+  })
+})
+
+
+
+describe('controls', () => {
+
+  it('renders state for the closed and locked buttons', () => {
+
+  })
+
+  it('changes the test of the button when the state changes', () => {
+
+  })
+
+  it('disables the close toggle button when the gate is closed', () => {
+
+  })
+
+  it('disables the lock toggle button when the gate is open', () => {
+
+  })
+})
